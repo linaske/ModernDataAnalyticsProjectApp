9e42d58ba3ff1df58d2159887d1ca6e73ac1624b
linaske <l.lescinskas@gmail.com>
2023-06-03 16:20:54 +0200

    updated data and layout

--- app.py
+++ app.py
@@ -14,24 +14,30 @@ app = dash.Dash(__name__)
 server = app.server
 
 # Set up layout
-app.layout = html.Div(children=[
-    html.H1('Crowdedness at Oude Markt'),
-    html.Div(id='noise-level', className='box'),
-    html.Div(id='highest-value', className='box'),
-    dcc.Graph(id='line-chart'),
-    dcc.RangeSlider(
-        id='time-selector',
-        marks={i: {'label': str(data.loc[i, 'timestamp'].time()), 'style': {
-                    'transform': 'rotate(-45deg)', 'white-space': 'nowrap'}} for i in range(0, len(data), len(data)//8)},
-        min=0,
-        max=len(data) - 1,
-        value=[0, len(data) - 1],
-        allowCross=False,
-        pushable=1
-    ),
-])
+app.layout = html.Div(
+    style={'font-family': 'Arial'},
+    children=[
+        html.H1('Crowdedness at Oude Markt'),
+        html.Div(id='noise-level', className='box'),
+        html.Div(id='highest-value', className='box'),
+        dcc.Graph(
+            id='line-chart',
+            style={'background-color': 'white'}
+        ),
+        dcc.RangeSlider(
+            id='time-selector',
+            marks={i: {'label': str(data.loc[i, 'timestamp'].time()), 'style': {
+                'transform': 'rotate(-45deg)', 'white-space': 'nowrap'}} for i in range(0, len(data), len(data) // 8)},
+            min=0,
+            max=len(data) - 1,
+            value=[0, len(data) - 1],
+            allowCross=False,
+            pushable=1,
+            className='slider'
+        ),
+    ])
 
-# Define the callbacks
+# Define callbacks
 @app.callback(
     dash.dependencies.Output('line-chart', 'figure'),
     dash.dependencies.Output('noise-level', 'children'),
@@ -49,16 +55,15 @@ def update_data(selected_indices):
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(
-        x=filtered_data['timestamp'], y=filtered_data['value'], mode='lines', name='Predicted Noise Level'))
+        x=filtered_data['timestamp'], y=filtered_data['value'], mode='lines', name='Predicted Noise Level', line=dict(width=3)))
 
     max_value = round(filtered_data['value'].max(), 2)
-    max_timestamp = filtered_data.loc[filtered_data['value'].idxmax(
-    ), 'timestamp']
+    max_timestamp = filtered_data.loc[filtered_data['value'].idxmax(), 'timestamp']
 
     mean_of_the_day = data['value'].mean()
     avg_value = round(filtered_data['value'].mean(), 2)
 
-    if avg_value >= 1.05 * mean_of_the_day:
+    if avg_value >= 1.025 * mean_of_the_day:
         noise_level = 'Busy'
         box_color = 'red'
     else:
@@ -66,30 +71,32 @@ def update_data(selected_indices):
         box_color = 'green'
 
     noise_level_box = html.Div(
-        style={'background-color': box_color,
-                'padding': '10px', 'color': '#FFFFFF'},
+        style={'background-color': box_color, 'padding': '10px', 'color': '#FFFFFF'},
         children=['Situation at Oude: ' + noise_level]
     )
 
     highest_value_box = html.Div(
-        style={'background-color': 'white',
-                'padding': '10px', 'color': 'black'},
-        children=['Highest Value: ' +
-                str(max_value) + ' (Time: ' + str(max_timestamp.time()) + ')']
+        style={'background-color': 'white', 'padding': '10px', 'color': 'black'},
+        children=['Highest Value: ' + str(max_value) + ' (Time: ' + str(max_timestamp.time()) + ')']
     )
 
-    fig.add_trace(go.Scatter(x=[max_timestamp], y=[
-                    max_value], mode='markers', name='Highest Value', marker=dict(color='red', size=8)))
-
-    return (
-        {
-            'data': [go.Scatter(x=filtered_data['timestamp'], y=filtered_data['value'], mode='lines', name='Predicted Noise Level'),
-                    go.Scatter(x=[max_timestamp], y=[max_value], mode='markers', name='Highest Value', marker=dict(color='red', size=8))],
-            'layout': go.Layout(xaxis=dict(title='Time'), yaxis=dict(title='Decibels'), showlegend=True)
-        },
-        noise_level_box,
-        highest_value_box
+    fig.update_layout(
+        xaxis=dict(
+            title='Time',
+            tickformat='%H:%M',
+            tickfont=dict(family='Arial'),
+            title_font=dict(family='Arial')
+        ),
+        yaxis=dict(title='Decibels (dB)', tickfont=dict(family='Arial')),
+        showlegend=True,
+        plot_bgcolor='white',
+        paper_bgcolor='white'
     )
+
+    fig.add_trace(go.Scatter(x=[max_timestamp], y=[max_value], mode='markers', name='Highest Value', marker=dict(color='red', size=12)))
+
+    return fig, noise_level_box, highest_value_box
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
